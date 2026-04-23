bd02aae8b435c534386c098ddf404f2dff9605db
Daniel <daniel.henriques.gamedev@gmail.com>
2026-04-23 16:01:10 +0100

    Class Cell implemtation

--- PowerCell/Cell.cs
+++ PowerCell/Cell.cs
@@ -7,6 +7,42 @@ namespace PowerCell
 {
     public class Cell
     {
-        
+        private float charge;
+
+        private Random rnd;
+
+        public string Name{get;}
+
+        public float Charge
+        {
+            get
+            {
+                return rnd.Next(0,201);
+            }
+
+        }
+
+        public int Level
+        {
+            get
+            {
+                return 1 + (int)Charge/40;
+            }
+        }
+
+        public Cell (string name)
+        {
+            Name = name;
+            charge = 200;
+        }
+
+        public void Consume (float amount)
+        {
+            charge -= amount;
+        }
+        public void Restore()
+        {
+            charge = 200;
+        }
     }
 }
